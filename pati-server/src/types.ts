56795a14ca9a5122c2f83dade318a0be5ac8990b
Learner Getready <learnergit@hymyilee.fi>
2023-12-30 14:40:36 +0200

    9.22

--- pati-server/src/types.ts
+++ pati-server/src/types.ts
@@ -1,4 +1,4 @@
-export type Diagnose = {
+export type Diagnosis = {
     code: string,
     name: string,
     latin?: string
@@ -8,10 +8,6 @@ export enum Gender {
     Male = "male",
     Female = "female",
     Other = "other",
-}
-
-export interface Entry {
-
 }
 
 export type Patient = {
@@ -27,3 +23,50 @@ export type Patient = {
 export type SafePatient = Omit<Patient, "ssn" | "entries">;
 
 export type NewPatient = Omit<Patient, "id">;
+
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+
+    diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+}
+
+export interface Discharge {
+    date: string;
+    criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+    type: "Hospital";
+    discharge: Discharge;
+}
+
+export interface SickLeave {
+    startDate: string;
+    endDate: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: 'OccupationalHealthcare';
+    employerName: string;
+    sickLeave?: SickLeave;
+}
+
+export type Entry =
+    | HospitalEntry
+    | OccupationalHealthcareEntry
+    | HealthCheckEntry;
